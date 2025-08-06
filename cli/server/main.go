9be6f106cfc39dfb3d92190cdc9a2d8a29b2d06c
Javier Garcia <zegaro@MacBook-Air-de-Javier.local>
2025-08-05 23:14:24 -0400

    add health route

--- cli/server/main.go
+++ cli/server/main.go
@@ -1,1 +1,12 @@
-package server
+package main
+
+import (
+	"github.com/Segian/little-sender/internal/api"
+)
+
+func main() {
+	router := api.NewRouter()
+	if err := router.Run(":8080"); err != nil {
+		panic("Failed to start server: " + err.Error())
+	}
+}
